28c29f0fc8c52f39a45126735b65abbde9491c21
philschonholzer <philip.schoenholzer@outlook.com>
2016-01-09 12:42:57 +0100

    PouchDB running in the main thread with memdown adapter

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var app = require('app');
 var BrowserWindow = require('browser-window');
+var PouchDB = require('pouchdb');
 
 var mainWindow = null;
 
@@ -10,6 +11,21 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
+    
+      var db = new PouchDB('mydb-idb',{db: require('memdown')});
+     
+      var remote = new PouchDB('http://127.0.0.1:5984/test_sync');
+
+      db.post({
+          name: 'Peter',
+          age: 17
+      }).then(function (response) {
+            db.sync(remote).on('complete', function (info) {
+                console.log("Synced!");
+            });
+      });
+    
+    
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
